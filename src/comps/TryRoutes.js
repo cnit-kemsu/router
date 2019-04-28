41d034ece1942795fc9c38ef6f027f8c0edff810
John Cooper <johnxcooper87@gmail.com>
2019-04-28 14:10:11 +0700

    update

--- src/comps/TryRoutes.js
+++
@@ -1,9 +0,0 @@
-export function TryRoutes({ defaultOutput, children }) {
-  if (children !== undefined) {
-    if (!Array.isArray(children)) return children;
-    for (const child of children) if (child !== undefined) return children;
-  }
-  return typeof defaultOutput === 'function'
-    ? defaultOutput()
-    : defaultOutput;
-}
