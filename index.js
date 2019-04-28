4acd03721b65c8ef29254396933fb93c0c369dbe
John Cooper <johnxcooper87@gmail.com>
2019-04-28 15:25:55 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { History } from './src/classes/History';
 export { Location } from './src/classes/Location';
 export { useRoute } from './src/hooks/useRoute';
-export { TryRoutes } from './src/comps/TryRoutes';
+export { useRoutes } from './src/comps/useRoutes';
