d99a2608b8c27b9b3da7d96f0abdd80f7f1057ef
John Cooper <johnxcooper87@gmail.com>
2019-05-13 14:28:58 +0700

    update

--- src/classes/History.js
+++ src/classes/History.js
@@ -15,6 +15,12 @@ export class History {
     History.updateEvent.publish();
   }
 
+  static update(path, search = {}, data) {
+    Location.search = search;
+    history.replaceState(data, undefined, (path || '/') + QS.stringify(search));
+    History.updateEvent.publish();
+  }
+
   static replace(path, search = {}, data) {
     Location.search = search;
     history.replaceState(data, undefined, (path || '/') + QS.stringify(search));
