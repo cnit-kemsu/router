558a5add4ac2bc20a84fc11105d83a1310612664
unknown <johnxcooper87@gmail.com>
2020-01-22 14:03:46 +0700

    update

--- src/classes/History.js
+++ src/classes/History.js
@@ -9,25 +9,50 @@ export class History {
     return history.state;
   }
 
+  static _historyStack = [];
+  static currentStateId = 0;
+
+  static _beforeRoute = null;
+  static beforeRoute() {
+    if (History._beforeRoute) History._beforeRoute();
+  }
+  static setBeforeRoute(value) {
+    if (value) History._beforeRoute = value;
+  }
+
+  static getPreviousState() {
+    return History._historyStack[History.currentStateId - 1];
+  }
+
   static push(path, search = {}, data) {
+
+    History.beforeRoute();
+    History._historyStack.push({
+      state: History.state,
+      path: Location.path,
+      search: Location.search,
+      stackId: History.currentStateId
+    });
+
     Location.search = search;
-    history.pushState(data, undefined, (path || '/') + QS.stringify(search));
+    History.currentStateId++;
+    history.pushState(data, document.title, (path || '/') + QS.stringify(search));
     History.updateEvent.publish();
   }
 
   static update(path, search = {}, data) {
     Location.search = search;
-    history.replaceState(data, undefined, (path || '/') + QS.stringify(search));
+    history.replaceState(data, document.title, (path || '/') + QS.stringify(search));
     History.updateEvent.publish();
   }
 
   static replace(path, search = {}, data) {
     Location.search = search;
-    history.replaceState(data, undefined, (path || '/') + QS.stringify(search));
+    history.replaceState(data, document.title, (path || '/') + QS.stringify(search));
   }
 }
 
-function handlePopstate() {
+function handlePopstate(event) {
   Location.search = QS.parse(location.search);
   History.updateEvent.publish();
 }
